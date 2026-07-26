3b0d9d4b885282e33916a14abc9fb79d1132cd14
jdmbusinessmail-cmyk <jdmbusinessmail@gmail.com>
2026-07-26 16:32:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("JDM Fitness Loaded Successfully!");
+// Welcome Message
+console.log("Welcome to JDM Fitness");
+
+// Join Button
+
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", function () {
+    alert("Welcome to JDM Fitness! 💪");
+});
+
+// Navbar Active Effect
+
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link => {
+    link.addEventListener("click", function () {
+
+        links.forEach(item => item.style.color = "white");
+
+        this.style.color = "red";
+    });
+});
