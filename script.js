c92f3f6fb145f6b6bb3577237ae9f03f4a51ff01
jdmbusinessmail-cmyk <jdmbusinessmail@gmail.com>
2026-07-26 16:02:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
+// Sticky Header
 
+window.addEventListener("scroll", () => {
+    const header = document.querySelector("header");
+
+    if(window.scrollY > 50){
+        header.style.background = "#000";
+    }else{
+        header.style.background = "rgba(0,0,0,.7)";
+    }
+});
+
+// Smooth Scroll
+
+document.querySelectorAll('nav a').forEach(anchor => {
+
+    anchor.addEventListener("click", function(e){
+
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href"))
+        .scrollIntoView({
+            behavior:"smooth"
+        });
+
+    });
+
+});
+
+// Contact Form
+
+const form = document.querySelector("form");
+
+if(form){
+
+form.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    alert("Thank You! Your message has been sent.");
+
+    form.reset();
+
+});
+
+}
